f2929e36e6569373ab00172c11d7ba9ebfca238d
hrutikpiprade <pipradehrutik@gmail.com>
2026-01-06 22:26:30 +0530

    leetcode-1161

---
+++ LeetCode/POTD/1161_Maximum_Level_Sum_of_a_Binary_Tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        int max = Integer.MIN_VALUE;
+        int ans=0;
+        int sum =0; 
+        int level =1;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int count = q.size();
+            sum =0;
+            for(int i=0; i<count ; i++){
+                TreeNode node = q.poll();
+                sum += node.val;
+                
+                if(node.left!=null) q.add(node.left);
+                if(node.right!=null) q.add(node.right);
+            }
+            if(sum > max){
+                max=sum;
+                ans=level;
+            }
+            level++;
+        }
+        return ans;
+    }
+}
